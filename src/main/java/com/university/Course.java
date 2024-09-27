820dfd598d8547d3b633ece73bc5a185d91bc66c
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-09-27 12:32:29 -0300

    Agregado de metodos para la clase Course

--- src/main/java/com/university/Course.java
+++ src/main/java/com/university/Course.java
@@ -8,23 +8,45 @@ public class Course {
     private int max_students;
     private int students_amount;
     
-    // =============== Constructor ========================
+    // =============== Constructor ==========================
     public Course(List<Student> students, String name, int max_students) {
-        this.students = students;
         this.name = name;
         this.max_students = max_students;
 
         if(students.equals(List.of())) { this.students_amount = 0;}
         else {
             for(Student st : students) {
-                students_amount += 1;
+                this.AddStudent(st);
             }
         }
     }
-    // ====================================================
+    // ======================================================
 
+    // Settting up al the gets. !============================
     public List<Student> getStudents() { return students; };
     public String getName() { return name; };
     public int getStudents_amount() { return students_amount; };
+    // ======================================================
+
+//---------------------------------------------------------------------
+
+    // CLASS METHODS ========================================
+    public void AddStudent(Student new_student) {
+        if(max_students <= students_amount) { return; }
+        if(new_student != null) {
+            students.add(new_student);
+            students_amount += 1;
+        }
+    }
+
+    public void RmvStudent(Student new_student) {
+        if(max_students <= students_amount) { return; }
+        if(new_student != null) {
+            students.remove(new_student);
+            students_amount -= 1;
+        }
+    }
     
+
+    // ======================================================
 }
